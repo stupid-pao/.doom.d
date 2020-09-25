4045a4baf8a7a0d21754c9f550a91862c67e521a
XQ <dahuaciliu@gmail.com>
2020-09-25 11:27:59 +0800

    update

--- init.el
+++ init.el
@@ -124,6 +124,7 @@
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
+       (dart +lsp)
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
